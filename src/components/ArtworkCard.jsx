262ad04523f210987b6bd6223a66e50e764c9bee
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-10-08 12:04:08 +0100

    update artwork card

--- src/components/ArtworkCard.jsx
+++ src/components/ArtworkCard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const ArtworkCard = ({
   artwork,
@@ -11,7 +9,10 @@ const ArtworkCard = ({
   isExhibitionPage,
 }) => {
   return (
-    <div className="max-w-md rounded overflow-hidden shadow-lg m-2 hover:shadow-xl transition-shadow duration-300 relative flex flex-col h-full">
+    <Link
+      to={`/artwork/${artwork.source}/${artwork.id}`}
+      className="max-w-md rounded overflow-hidden shadow-lg m-2 hover:shadow-2xl hover:scale-102 transition-transform transform duration-300 relative flex flex-col h-full"
+    >
       {/* image section */}
       <img
         src={artwork.image_url}
@@ -22,55 +23,51 @@ const ArtworkCard = ({
       {/* card content */}
       <div className="p-4 flex-grow flex flex-col justify-between">
         <div>
-          <div className="font-bold text-sm sm:text-sm md:text-lg lg:text-xl font-bold mb-2 flex justify-between items-center">
+          <div className="font-bold text-sm sm:text-sm md:text-lg lg:text-xl font-bold mb-2">
             {artwork.title}
-            <Link
-              to={`/artwork/${artwork.source}/${artwork.id}`}
-              className="text-blue-600"
-            >
-              <span className="sr-only">More info about {artwork.title}</span>
-              <FontAwesomeIcon
-                icon={faInfoCircle}
-                className="text-blue-600 cursor-pointer ml-2"
-              />
-            </Link>
           </div>
           <p className="font-semibold text-sm mb-2">
-            {artwork.date ? `${artwork.date}` : ""}{" "}
+            {artwork.date ? `${artwork.date}` : ""}
           </p>
 
-          <p className="text-sm text-gray-700">{artwork.artist} </p>
-        </div>
-
-        {/* add/remove from exhib buttons */}
-        <div className="mt-2">
-          {!isExhibitionPage ? (
-            isInExhibition ? (
-              <button
-                disabled
-                className="w-full bg-gray-500 text-[12px] text-white py-2 px-2 rounded cursor-not-allowed"
-              >
-                Already in Exhibition
-              </button>
-            ) : (
-              <button
-                onClick={() => onAddToExhibition(artwork)}
-                className="w-full bg-blue-600 hover:bg-blue-700 text-[12px] text-white py-2 px-2 rounded"
-              >
-                Add to Exhibition
-              </button>
-            )
-          ) : (
-            <button
-              onClick={() => onRemoveFromExhibition(artwork)}
-              className="w-full bg-red-600 hover:bg-red-700 text-[12px] text-white py-2 px-2 rounded"
-            >
-              Remove from Exhibition
-            </button>
-          )}
+          <p className="text-sm text-gray-700">{artwork.artist}</p>
         </div>
       </div>
-    </div>
+
+      {/* add/remove from exhib buttons */}
+      <div className="mt-2">
+        {!isExhibitionPage ? (
+          isInExhibition ? (
+            <button
+              disabled
+              className="w-full bg-gray-500 text-[12px] text-white py-2 px-2 rounded cursor-not-allowed"
+            >
+              Already in Exhibition
+            </button>
+          ) : (
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                onAddToExhibition(artwork);
+              }}
+              className="w-full bg-blue-600 hover:bg-blue-700 text-[12px] text-white py-2 px-2 rounded"
+            >
+              Add to Exhibition
+            </button>
+          )
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              onRemoveFromExhibition(artwork);
+            }}
+            className="w-full bg-red-600 hover:bg-red-700 text-[12px] text-white py-2 px-2 rounded"
+          >
+            Remove from Exhibition
+          </button>
+        )}
+      </div>
+    </Link>
   );
 };
 
